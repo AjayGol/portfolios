0afd6a02bafe5d784596e797571b2a6c75a72262
AjayGol <ajaygoldev@gmail.com>
2024-04-13 11:58:42 +0530

    education details changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,6 @@ export const headerData = {
   title: "Hello! I'am",
   image:
     "https://github.com/AjayGol/portfolios/assets/77920960/532d87b6-8884-4bd9-ad32-e1c2f35ef1d5",
+  avtarImage :'https://github.com/AjayGol/portfolios/assets/77920960/c64b5bca-fef6-4528-96a0-f12763dbeb19',
   resumePdf: resume,
 };
